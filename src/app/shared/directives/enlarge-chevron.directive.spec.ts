a26505f89cf3da296561d1b593142b815c265bbb
szymon-rogus <sszymek98@gmail.com>
2021-05-16 16:42:30 +0200

    Removing spec files from shared

--- src/app/shared/directives/enlarge-chevron.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EnlargeChevronDirective } from './enlarge-chevron.directive';
-
-describe('EnlargeChevronDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EnlargeChevronDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/directives/highlight.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HighlightDirective } from './highlight.directive';
-
-describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    // @ts-ignore
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
